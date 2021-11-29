e76ab6e542d5d8df6b4fa1cef7d68d1fdf5365ff
Mithu-12 <mithuvowmick96@gmail.com>
2021-11-29 22:43:34 +0800

    last

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+        $(window).scroll(function(){
+            if(this.scrollY> 20){
+                $('.navbar1').addClass('sticky');
+            }
+            else{
+                $('.navbar1').removeClass('sticky');
+            }
+        });
+        $('.menu-btn').click(function(){
+            $('.navbar1 .menu').toggleClass('active')
+            $('.menu-btn i').toggleClass('active')
+        })
+    })
